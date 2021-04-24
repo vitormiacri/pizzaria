6bc30e415e2a0d35f6e7c27573fadf968287240c
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-23 23:15:24 -0300

    test: ensure AxiosHttpClient returns correct values

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,36 @@
+import { mockHttpRequest } from '@/data/test/mock-http';
+import { mockAxios } from '@/infra/test/mock-axios';
+import { AxiosHttpClient } from './axios-http-client';
+import axios from 'axios';
+
+jest.mock('axios');
+
+type SutTypes = {
+  sut: AxiosHttpClient;
+  mockedAxios: jest.Mocked<typeof axios>;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
+  return {
+    sut,
+    mockedAxios,
+  };
+};
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct values', async () => {
+    const request = mockHttpRequest();
+    const { sut, mockedAxios } = makeSut();
+
+    await sut.request(request);
+
+    expect(mockedAxios.request).toHaveBeenCalledWith({
+      url: request.url,
+      data: request.body,
+      headers: request.headers,
+      method: request.method,
+    });
+  });
+});
