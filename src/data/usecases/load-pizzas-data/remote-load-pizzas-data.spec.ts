1fc5ebb9d39d0b6eac00f4d3facc313b0a0189f4
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-23 22:00:57 -0300

    test: ensure RemoteLoadPizzas throw a UnexpectError if HttpClient return 404

--- src/data/usecases/load-pizzas-data/remote-load-pizzas-data.spec.ts
+++ src/data/usecases/load-pizzas-data/remote-load-pizzas-data.spec.ts
@@ -1,7 +1,15 @@
+import { HttpStatusCode } from '@/data/protocols/http/http-client';
 import { HttpClientSpy } from '@/data/protocols/test/mock-http';
 import { RemoteLoadPizzasData } from '@/data/usecases/load-pizzas-data/remote-load-pizzas-data';
+import { UnexpectedError } from '@/domain/errors/unexpected-error';
+import faker from 'faker';
 
-const makeSut = (url: string) => {
+type SutTypes = {
+  sut: RemoteLoadPizzasData;
+  httpClientSpy: HttpClientSpy<RemoteLoadPizzasData.Model>;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
   const httpClientSpy = new HttpClientSpy();
   const sut = new RemoteLoadPizzasData(url, httpClientSpy);
   return {
@@ -9,12 +17,22 @@ const makeSut = (url: string) => {
     httpClientSpy,
   };
 };
+
 describe('RemoteLoadPizzasData', () => {
   test('Should be call HttpClient with correct URL and Method', async () => {
-    const url = 'any_url';
+    const url = faker.internet.url();
     const { sut, httpClientSpy } = makeSut(url);
     await sut.loadData();
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe('get');
+  });
+
+  test('Should throw UnexpectedError if HttpClient return 404', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+    const promise = sut.loadData();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
