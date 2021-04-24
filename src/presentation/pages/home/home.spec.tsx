af850d3fb84d269b531da2fbdf1515ed57e14634
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-24 14:35:12 -0300

    test: ensure Home page go to Order page if button clicked

---
+++ src/presentation/pages/home/home.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import Home from './home';
+import { Router } from 'react-router';
+
+const history = createMemoryHistory({ initialEntries: ['/home'] });
+
+describe('Home Component', () => {
+  test('Should go to Order page', () => {
+    const { getByTestId } = render(
+      <Router history={history}>
+        <Home />
+      </Router>
+    );
+    const button = getByTestId('order-init');
+    fireEvent.click(button);
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe('/order');
+  });
+});
