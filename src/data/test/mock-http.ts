9f13456b5c3a843a654a7bba5ec1f83e1d55c608
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-23 23:15:13 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,9 +1,17 @@
+import faker from 'faker';
 import {
   HttpClient,
   HttpRequest,
   HttpResponse,
   HttpStatusCode,
-} from '../http/http-client';
+} from '@/data/protocols/http/http-client';
+
+export const mockHttpRequest = (): HttpRequest => ({
+  url: faker.internet.url(),
+  method: faker.random.arrayElement(['get', 'post', 'put', 'delete']),
+  body: faker.random.objectElement(),
+  headers: faker.random.objectElement(),
+});
 
 export class HttpClientSpy<R = any> implements HttpClient<R> {
   url?: string;
