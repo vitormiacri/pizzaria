da31f6e658941808ac840bdfa4978eb691289cb2
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-24 14:21:52 -0300

    chore: config for images import

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,13 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
     ],
   },
   devServer: {
