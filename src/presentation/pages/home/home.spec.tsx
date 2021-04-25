2c57212d81bc2ceadfe799d53f272c0c791c21d5
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-25 09:51:47 -0300

    test: ensure go to Checkout after click

--- src/presentation/pages/home/home.spec.tsx
+++ src/presentation/pages/home/home.spec.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import React, { useContext } from 'react';
+import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import { renderWithHistory } from '@/presentation/test/render-help';
 import Home from './home';
-import { Router } from 'react-router';
 
 const history = createMemoryHistory({ initialEntries: ['/home'] });
 
+const makeSut = () => {
+  const sut = renderWithHistory({
+    Page: () => Home({ children: <Home /> }),
+    history,
+  });
+  return sut;
+};
+
 describe('Home Component', () => {
+  afterEach(cleanup);
+
   test('Should go to Order page', () => {
-    const { getByTestId } = render(
-      <Router history={history}>
-        <Home />
-      </Router>
-    );
-    const button = getByTestId('order-init');
-    fireEvent.click(button);
+    const sut = makeSut();
+    const button = sut.getByTestId('order-button');
+    waitFor(() => fireEvent.click(button));
     expect(history.length).toBe(2);
     expect(history.location.pathname).toBe('/order');
+  });
+
+  test('Should go to Checkout page', () => {
+    const sut = makeSut();
+    const button = sut.getByTestId('checkout-button');
+    waitFor(() => fireEvent.click(button));
+    expect(history.length).toBe(3);
+    expect(history.location.pathname).toBe('/checkout');
   });
 });
